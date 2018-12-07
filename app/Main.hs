73e0af2b30c355f25286fc3b2869c683d169bfa6
Pedro Queiroga <psq@cin.ufpe.br>
2018-12-07 06:17:06 -0300

    ops agora s

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Front
 
 main :: IO ()
-main = putStrLn "Bem-vindo ao hasky-compiler\n" >> (compute =<< readLn)
+main = putStrLn "Bem-vindo ao hasky-compiler\n" >> (compute =<< getLine)
